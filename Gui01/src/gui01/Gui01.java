00f7bf406f9f9474aa6d005f2f1ff371ab67a664
adamiv <valmor_adami_jr@whirlpool.com>
2020-07-09 20:14:31 -0300

    adicionando o gui01

---
+++ Gui01/src/gui01/Gui01.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui01;
+
+/**
+ *  awt e swing sao os pacotes graficos do Java.
+ *  Gerenciadores de layout.
+ *  Swing e components de inteface.
+ * @author old_adam
+ */
+public class Gui01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
